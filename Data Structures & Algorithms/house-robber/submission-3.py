a06df66515d0ec5b99cebd0a77805b42aaabe017
MOHAN CHANDRA S S <155703431+mohanchandrass@users.noreply.github.com>
2026-07-21 19:49:40 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)<=2:
+            return max(nums)
+
+        a = nums[0]
+        b = max(nums[0],nums[1])
+
+        for i in range(2,len(nums)):
+            print(a,b)
+            a,b = b, max(a+nums[i],b)
+            print("nums[i] = ",nums[i])
+            print(a,b)
+        
+        return max(a,b) 
+        
