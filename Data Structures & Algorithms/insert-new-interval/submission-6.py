bdaf525e9d69340c0d37fcc40891771d607fb4af
MOHAN CHANDRA S S <155703431+mohanchandrass@users.noreply.github.com>
2026-08-01 23:51:02 +0530

    Add: insert-new-interval - submission-6

---
+++ Data Structures & Algorithms/insert-new-interval/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key = lambda x : x[0])
+        merged = [intervals[0]]
+        for start,end in intervals[1:]:
+            merged_end = merged[-1][1]
+            if start<=merged_end:
+                merged[-1][1] = max(merged_end,end)
+            else:
+                merged.append([start,end])
+        
+        return merged
+        
