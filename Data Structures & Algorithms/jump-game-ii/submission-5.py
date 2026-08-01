a7fa2b1e25d0c3e294b9c3fac07a72de82986c02
MOHAN CHANDRA S S <155703431+mohanchandrass@users.noreply.github.com>
2026-08-01 15:02:30 +0530

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        goal = len(nums)-1
+        jump = 0
+        count = 0
+        maxreach = 0
+        if jump>=goal:
+            return count
+        for i in range(len(nums)-1):
+            curr = i + nums[i]
+            maxreach = max(curr,maxreach)
+            if i == jump:
+                count+=1
+                jump = maxreach
+            if jump>=goal:
+                return count
+        
+        return count
+
+
+        
